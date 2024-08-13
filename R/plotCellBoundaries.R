03685f7c0da49bbe8f7d54921222d62cba62239d
vjcitn <stvjc@channing.harvard.edu>
2024-08-13 12:24:44 -0400

    some updates

---
+++ R/plotCellBoundaries.R
@@ -0,0 +1,48 @@
+namesInBox = function (xsce, xlim, ylim) 
+{
+    xc = xsce$x_centroid
+    yc = xsce$y_centroid
+    inds = which(xc >= xlim[1] & xc <= xlim[2] & yc >= ylim[1] & 
+        yc <= ylim[2])
+    unique(colnames(xsce[, inds]))
+}
+
+#' restrict XenSCE to cells with centroids in specified rectangle
+#' @param xsce XenSCE instance
+#' @param xlim numeric(2)
+#' @param ylim numeric(2)
+#' @export
+clip_rect = function(xsce, xlim, ylim) {
+  nn = namesInBox(xsce, xlim, ylim)
+  stopifnot(length(nn)>0)
+  xsce[, nn]
+}
+
+#' render boundaries of cells with optional centroid positions and transcript positions
+#' @param xsce XenSCE instance
+#' @param add_cent logical(1)
+#' @param cent_col character(1) default to "red"
+#' @param cent_cex numeric(1) default to 0.2
+#' @param add_tx logical(1)
+#' @param tx_cent logical(1)
+#' @examples
+#' dem = build_panc_subset()
+#' plotCellBoundaries(clip_rect(dem, xlim=c(600,850), ylim=c(500,750)))
+#' @export
+plotCellBoundaries = function(xsce, add_cent=TRUE, cent_col="red", cent_cex=.2, add_tx=TRUE,
+   tx_cex=.1) {
+  bb = getCellBoundaries(xsce)
+  bb = bb[bb$cell_id %in% colnames(xsce),]
+  xlim = range(bb$vertex_x)
+  ylim = range(bb$vertex_y)
+  plot(min(xlim), min(ylim), pch=" ", xlim=xlim, ylim=ylim, xlab="x", ylab="y")
+  sbb = split(as(bb, "data.frame"), bb$cell_id)
+  for (i in seq_len(length(sbb))) polygon(sbb[[i]]$vertex_x, sbb[[i]]$vertex_y)
+  if (add_cent) points(xsce$x_centroid, xsce$y_centroid, cex=cent_cex, col=cent_col)
+  if (add_tx) points(getTranscripts(xsce)$x_location, getTranscripts(xsce)$y_location, cex=tx_cex)
+  invisible(NULL)
+}
+
+#library(XenSCE)
+#x = build_panc_subset()
+#plotCellBoundaries(clip_rect(x, xlim=c(200,450), ylim=c(800,1000)))
