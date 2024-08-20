d9ae9c00b09b366bb6dc03b17764a3a936adda5c
vjcitn <stvjc@channing.harvard.edu>
2024-08-20 08:15:39 -0400

    vignette includes some 5k data references, cache_xen_prost etc.

--- R/cache_mtif.R
+++ R/cache_mtif.R
@@ -49,3 +49,71 @@ cache_sfeLung = function(cache=BiocFileCache::BiocFileCache(),
  bfcadd(cache, rname=basename(url), fpath=url, action="copy", download=TRUE)
 }
 
+#' cache and/or retrieve path to xenium human prostate adenocarcinoma, 5k genes
+#' @param cache defaults to BiocFileCache::BiocFileCache()
+#' @param url location where tiff file can be retrieved
+#' @note Retrieves path to a 912MB zip file.  This
+#' includes a 'saved' HDF5-backed SpatialExperiment and
+#' parquet files for cell, nucleus, and transcript coordinates.
+#' The example shows how these can be assembled into a
+#' XenSPEP instance.
+#' @examples
+#' if (interactive()) {
+#'  td = tempdir()
+#'  pzip_path = cache_xen_prost()
+#'  dir.create(xpw <- file.path(td, "xen_prost_work"))
+#'  unzip(pzip_path, exdir=xpw)
+#'  prost = HDF5Array::loadHDF5SummarizedExperiment(file.path(xpw, "xen_prost"))
+#'  prost
+#'  cd = getwd()
+#'  setwd(xpw)
+#'  prost = loadGeometry(prost)
+#'  print(slot(prost, "cbtab") |> head() |> dplyr::collect())
+#'  print(slot(prost, "txtab") |> dplyr::count() |> dplyr::collect())
+#'  plot(SpatialExperiment::spatialCoords(prost), pch=".",
+#'    main="10x FFPE Human prostate adenocarcinoma sample")
+#'  setwd(cd)
+#' }
+#' @export
+cache_xen_prost = function(cache=BiocFileCache::BiocFileCache(), 
+   url="https://mghp.osn.xsede.org/bir190004-bucket01/BiocXenData/xen_prost.zip") {
+ chk = bfcquery(cache, "BiocXenData/xen_prost.zip")
+ n = nrow(chk)
+ if (n>=1) return(chk[n,]$rpath)
+ bfcadd(cache, rname=basename(url), fpath=url, action="copy", download=TRUE)
+}
+
+#' cache and/or retrieve path to xenium human primary melanoma, 5k genes
+#' @param cache defaults to BiocFileCache::BiocFileCache()
+#' @param url location where tiff file can be retrieved
+#' @note Retrieves path to a 1.05GB zip file.  The zip file
+#' includes a 'saved' HDF5-backed SpatialExperiment and
+#' parquet files for cell, nucleus, and transcript coordinates.
+#' The example shows how these can be assembled into a
+#' XenSPEP instance.
+#' @examples
+#' if (interactive()) {
+#'  td = tempdir()
+#'  pzip_path = cache_xen_sk()
+#'  dir.create(xpw <- file.path(td, "xen_sk_work"))
+#'  unzip(pzip_path, exdir=xpw)
+#'  mel = HDF5Array::loadHDF5SummarizedExperiment(file.path(xpw, "xen_sk"))
+#'  mel
+#'  cd = getwd()
+#'  setwd(xpw)
+#'  mel = loadGeometry(mel)
+#'  print(slot(mel, "cbtab") |> head() |> dplyr::collect())
+#'  print(slot(mel, "txtab") |> dplyr::count() |> dplyr::collect())
+#'  plot(SpatialExperiment::spatialCoords(mel), pch=".",
+#'    main="10x FFPE Human dermal melanoma sample")
+#'  setwd(cd)
+#' }
+#' @export
+cache_xen_sk = function(cache=BiocFileCache::BiocFileCache(), 
+   url="https://mghp.osn.xsede.org/bir190004-bucket01/BiocXenData/xen_sk_spep.zip") {
+ chk = bfcquery(cache, "BiocXenData/xen_sk_spep.zip")
+ n = nrow(chk)
+ if (n>=1) return(chk[n,]$rpath)
+ bfcadd(cache, rname=basename(url), fpath=url, action="copy", download=TRUE)
+}
+
